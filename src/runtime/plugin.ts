7e08ed7310fd44e3de6e16182e4f24131c786937
Yisrae <yisrae@fedorae.com>
2022-07-15 09:36:37 +0100

    Update plugin.ts: Add icon use statement to export

--- src/runtime/plugin.ts
+++ src/runtime/plugin.ts
@@ -3,9 +3,8 @@ import UIkit from 'uikit';
 import Icons from 'uikit/dist/js/uikit-icons';
 import 'uikit/dist/css/uikit.min.css';
 
-UIkit.use(Icons);
-
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.$UIkit = UIkit;
+  nuxtApp.$UIkit.use(Icons);
   nuxtApp.provide("UIkit", UIkit);
 })
